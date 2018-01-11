f57757dea399ad56f50b7ceeb4e1f7a7cec417b2
C1 <c1@gjun.com>
2018-01-11 14:18:35 +0800

    2

--- app/src/main/java/com/wilson/a2018011004/MainActivity.java
+++ app/src/main/java/com/wilson/a2018011004/MainActivity.java
@@ -1,9 +1,13 @@
 package com.wilson.a2018011004;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -28,74 +32,94 @@ import javax.xml.parsers.SAXParserFactory;
 public class MainActivity extends AppCompatActivity {
     ListView lv;
     ArrayAdapter<String> adapter;
+    MyHandler dataHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.listView);
-
-    }
-    public void click1(View v)
-    {
-        new Thread(){
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void run() {
-                super.run();
-                String str_url = "https://www.mobile01.com/rss/hottopics.xml";
-                //https://www.mobile01.com/rss/news.xml
-                //123
-                //567
-                URL url = null;
-                try {
-                    url = new URL(str_url);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.connect();
-                    InputStream inputStream = conn.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(inputStream);
-                    BufferedReader br = new BufferedReader(isr);
-                    StringBuilder sb = new StringBuilder();
-                    String str;
-
-                    while ((str = br.readLine()) != null)
-                    {
-                        sb.append(str);
-                    }
-                    String str1 = sb.toString();
-                    Log.d("NET-WebHtml", str1);
-                    final MyHandler dataHandler = new MyHandler();
-                    SAXParserFactory spf = SAXParserFactory.newInstance();
-                    SAXParser sp = spf.newSAXParser();
-                    XMLReader xr = sp.getXMLReader();
-                    xr.setContentHandler(dataHandler);
-                    xr.parse(new InputSource(new StringReader(str1)));
-
-                    br.close();
-                    isr.close();
-                    inputStream.close();
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            adapter = new ArrayAdapter<String>(MainActivity.this,
-                                    android.R.layout.simple_list_item_1, dataHandler.titles);
-                            lv.setAdapter(adapter);
-                        }
-                    });
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (SAXException e) {
-                    e.printStackTrace();
-                } catch (ParserConfigurationException e) {
-                    e.printStackTrace();
-                }
-
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it = new Intent(MainActivity.this, DetailActivity.class);
+                it.putExtra("link", dataHandler.newsItems.get(i).link);
+                startActivity(it);
             }
-        }.start();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_reload:
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        String str_url = "https://www.mobile01.com/rss/news.xml";
+                        URL url = null;
+                        try {
+                            url = new URL(str_url);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.connect();
+                            InputStream inputStream = conn.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(inputStream);
+                            BufferedReader br = new BufferedReader(isr);
+                            StringBuilder sb = new StringBuilder();
+                            String str;
+
+                            while ((str = br.readLine()) != null) {
+                                sb.append(str);
+                            }
+                            String str1 = sb.toString();
+                            Log.d("NET", str1);
+                            dataHandler = new MyHandler();
+                            SAXParserFactory spf = SAXParserFactory.newInstance();
+                            SAXParser sp = spf.newSAXParser();
+                            XMLReader xr = sp.getXMLReader();
+                            xr.setContentHandler(dataHandler);
+                            xr.parse(new InputSource(new StringReader(str1)));
+
+                            br.close();
+                            isr.close();
+                            inputStream.close();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String data[] = new String[dataHandler.newsItems.size()];
+                                    for (int i = 0; i < data.length; i++) {
+                                        data[i] = dataHandler.newsItems.get(i).title;
+                                    }
+                                    adapter = new ArrayAdapter<String>(MainActivity.this,
+                                            android.R.layout.simple_list_item_1, data);
+                                    lv.setAdapter(adapter);
+                                }
+                            });
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (SAXException e) {
+                            e.printStackTrace();
+                        } catch (ParserConfigurationException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }.start();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
